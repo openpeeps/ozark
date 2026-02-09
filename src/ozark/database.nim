19b9173667b8d502632c804d63d433e325047737
George Lemon <georgelemon@protonmail.com>
2026-02-09 10:35:05 +0200

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/ozark/database.nim
+++ src/ozark/database.nim
@@ -1,3 +1,9 @@
+# A magical ORM for the Nim language
+#
+# (c) 2026 George Lemon | MIT License
+#          Made by Humans from OpenPeeps
+#          https://github.com/openpeeps/ozark
+
 import std/[macros, net, strutils, tables]
 
 import pkg/threading/once
