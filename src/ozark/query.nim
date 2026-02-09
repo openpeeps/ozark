698b299bc6920d04ac5dcb617bc7f5c9b6293e60
George Lemon <georgelemon@protonmail.com>
2026-02-09 11:03:36 +0200

    handle `*` together with `instantRows`
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/ozark/query.nim
+++ src/ozark/query.nim
@@ -141,20 +141,29 @@ block:
     isEmpty = true
     results: Collection[$2]
     cols: DBColumns = @[]
-    colNames: seq[string]
+    colKeys: seq[string]
+  let colNames = [$3]
   for row in $4(dbcon, cols, SqlQuery("$1")):
     isEmpty = isEmpty and row.len == 0
-    if isEmpty: continue
-    if colNames.len == 0:
-      colNames = cols.mapIt(it.name)
+    if isEmpty: continue # skip empty rows
+    if colKeys.len == 0:
+      colKeys = cols.mapIt(it.name) # extract column names from DBColumns
     var inst = new($2)
     for fName, fValue in inst[].fieldPairs():
-      if fName in colNames:
-        fValue = row[colNames.find(fName)]
-      else:
-        raise newException(OzarkModelDefect, "Model field `$2." & fName & "` does not have a corresponding column in the SQL result")
+      if colNames[0] != "*" and fName in colNames:
+        # first check if the column name is in the list of
+        # selected columns, then find its index and assign the value from the row
+        if fName in colKeys:
+          fValue = row[colKeys.find(fName)]
+        else:
+          raise newException(OzarkModelDefect,
+            "Model field `$2." & fName & "` does not have a corresponding column in the SQL result")
+      elif colNames[0] == "*":
+        if fName in colKeys:
+          fValue = row[colKeys.find(fName)]
     results.entries.add(inst)
-  results""" % [$parsedSql, $(getTypeImpl(m)[1]), assigns.join("\n    "), getRowProcName]
+  results""" % [$parsedSql, $(getTypeImpl(m)[1]),
+          colNames.mapIt("\"" & it & "\"").join(","), getRowProcName]
     result = macros.parseStmt(runtimeCode) # parse the generated code into a NimNode
   except SqlParseError as e:
     raise newException(OzarkModelDefect, "SQL Parsing Error: " & e.msg)
